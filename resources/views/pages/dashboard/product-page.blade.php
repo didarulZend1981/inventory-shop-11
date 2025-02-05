43855acb8b037bf17d9cd6797647e11513c717ca
mohammad didarul islam <didarul1981@gmail.com>
2025-02-06 02:10:43 +0600

    create modal

--- resources/views/pages/dashboard/product-page.blade.php
+++ resources/views/pages/dashboard/product-page.blade.php
@@ -3,5 +3,6 @@
 @include('components.product.product-list')
 @include('components.product.product-delete')
 @include('components.product.product-update')
+@include('components.product.product-create')
 
 @endsection
