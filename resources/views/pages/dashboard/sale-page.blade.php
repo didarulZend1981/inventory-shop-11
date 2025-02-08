e1d1e348557ec9f69464d24a945a7570253cec54
mohammad didarul islam <didarul1981@gmail.com>
2025-02-09 00:22:59 +0600

    product add

--- resources/views/pages/dashboard/sale-page.blade.php
+++ resources/views/pages/dashboard/sale-page.blade.php
@@ -13,7 +13,7 @@
 
 
 
-
+              @include('components.invoice.invoice-form.product-add-form')
 
               @include('components.invoice.invoice-form.customer-create')
 
@@ -24,6 +24,43 @@
 
 <script>
 
+
+function add() {
+   let PId= document.getElementById('PId').value;
+   let PName= document.getElementById('PName').value;
+   let PPrice=document.getElementById('PPrice').value;
+   let PQty= document.getElementById('PQty').value;
+   let PTotalPrice=(parseFloat(PPrice)*parseFloat(PQty)).toFixed(2);
+   if(PId.length===0){
+       errorToast("Product ID Required");
+   }
+   else if(PName.length===0){
+       errorToast("Product Name Required");
+   }
+   else if(PPrice.length===0){
+       errorToast("Product Price Required");
+   }
+   else if(PQty.length===0){
+       errorToast("Product Quantity Required");
+   }
+   else{
+       let item={product_name:PName,product_id:PId,qty:PQty,sale_price:PTotalPrice};
+       InvoiceItemList.push(item);
+       console.log(InvoiceItemList);
+       $('#create-modal').modal('hide')
+       ShowInvoiceItem();
+   }
+}
+
+
+
+
+function addModal(id,name,price) {
+    document.getElementById('PId').value=id
+    document.getElementById('PName').value=name
+    document.getElementById('PPrice').value=price
+    $('#create-modal').modal('show')
+}
 
 </script>
 
